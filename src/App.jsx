2af209d5cbb2ab1aa7e0628d893b1013517880b8
andikadwikidd <wiqy0106@gmail.com>
2023-06-29 13:56:58 +0700

    Functional Component and Props

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Button from "./components/buttons/button";
+
+function App() {
+  return (
+    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+      <div className="flex gap-x-3">
+        <Button variant="bg-green-600">Login</Button>
+        <Button variant="bg-slate-600">Register</Button>
+        <Button></Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/buttons/button.jsx
@@ -0,0 +1,12 @@
+const Button = ({ children = "...", variant = "bg-black" }) => {
+  return (
+    <button
+      className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
+      type="submit"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
