b627a6e608701559918a2222c538c246359cf291
andikadwikidd <wiqy0106@gmail.com>
2023-07-03 22:27:46 +0700

    Rendering List

--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -2,7 +2,7 @@ import Button from "../Elements/button";
 
 const CardProduct = ({ children }) => {
   return (
-    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2">
+    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2 flex flex-col justify-between">
       {children}
     </div>
   );
@@ -15,12 +15,12 @@ const Header = ({ image }) => {
     </a>
   );
 };
-const Body = ({ children, title }) => {
+const Body = ({ children, name }) => {
   return (
-    <div className="px-5 pb-5">
+    <div className="px-5 pb-5 h-full">
       <a href="">
-        <h5 className="text-xl font-semibold tracking-tight text-white">
-          {title}
+        <h5 className="text-xl font-semibold tracking-tight text-white mb-3">
+          {name}
         </h5>
         <p className="text-m text-white">{children}</p>
       </a>
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,30 +1,37 @@
 import Button from "../components/Elements/button";
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    image: "images/product1.jpg",
+    name: "Smartphone 1",
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio, fugit
+    aut! Error nisi asperiores harum, magnam illo repellat, consectetur
+    enim reprehenderit minus culpa sequi odio dolores. Quas consectetur
+    cupiditate perferendis.`,
+  },
+  {
+    id: 2,
+    image: "images/product1.jpg",
+    name: "Smartphone 2",
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio, fugit aut!`,
+  },
+];
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="images/product1.jpg" />
-        <CardProduct.Body title="Smartphone">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio, fugit
-          aut! Error nisi asperiores harum, magnam illo repellat, consectetur
-          enim reprehenderit minus culpa sequi odio dolores. Quas consectetur
-          cupiditate perferendis.
-        </CardProduct.Body>
-        <CardProduct.Footer price={"Rp. 7.000.000"} />
-      </CardProduct>
-
-      <CardProduct>
-        <CardProduct.Header image="images/product1.jpg" />
-        <CardProduct.Body title="Smartphone">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio, fugit
-          aut! Error nisi asperiores harum, magnam illo repellat, consectetur
-          enim reprehenderit minus culpa sequi odio dolores. Quas consectetur
-          cupiditate perferendis.
-        </CardProduct.Body>
-        <CardProduct.Footer price={"Rp. 7.000.000"} />
-      </CardProduct>
+      {products.map((product) => {
+        return (
+          <CardProduct key={product.id}>
+            <CardProduct.Header image={product.image} />
+            <CardProduct.Body name={product.name}>
+              {product.description}
+            </CardProduct.Body>
+            <CardProduct.Footer price={product.price} />
+          </CardProduct>
+        );
+      })}
     </div>
   );
 };
